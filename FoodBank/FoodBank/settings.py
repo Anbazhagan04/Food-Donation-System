520d120b405de2e9369f34debb63f723f276f737
Anbazhagan04 <75233615+Anbazhagan04@users.noreply.github.com>
2023-02-18 22:51:41 +0530

    Edited
    Imports

--- FoodBank/FoodBank/settings.py
+++ FoodBank/FoodBank/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p#xylf*v67dvw^1k#yv@ie!xlzdqo8$1j96$4hmk2#@q*#w6yc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
